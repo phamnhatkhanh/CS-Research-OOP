8003c88cdcbda41ca8f90f4a06adc738ed3ca9b9
phamnhatkhanh <khanhhcm4@gmail.com>
2023-12-13 22:47:26 +0700

    Project: update package

--- server_oop_search/server.js
+++ server_oop_search/server.js
@@ -5,6 +5,8 @@ const http = require("http");
 
 const app = require("./app");
 
+const neo4j = require('neo4j-driver');
+
 const server = http.createServer(app);
 
 process.on("uncaughtException", (err) => {
@@ -14,6 +16,38 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 dotenv.config({ path: "./config.env" });
+
+// npm install --save neo4j-driver
+// node example.js
+
+const driver = neo4j.driver('bolt://44.213.126.205:7687',
+                  neo4j.auth.basic('neo4j', 'bunches-fireball-preserver'), 
+                  {/* encrypted: 'ENCRYPTION_OFF' */});
+
+const query =
+  `
+  MATCH path = shortestPath((node1:Content{name:'module-oriented programming'})-[*]-(node2:Content{name:'Ưu điểm'}))
+WITH nodes(path) AS nodesOnPath
+WITH nodesOnPath, [node IN nodesOnPath | CASE WHEN 'Title_Lesson' IN labels(node) THEN node.ID ELSE NULL END] AS titleLessonNodeIDs
+RETURN titleLessonNodeIDs;
+  `;
+
+// const params = {"limit": "10"};
+
+const session = driver.session({database:"neo4j"});
+
+session.run(query)
+  .then((result) => {
+    result.records.forEach((record) => {
+      console.log(record._fields[0]);
+    });
+    session.close();
+    driver.close();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 
 const DB = process.env.DATABASE;
 mongoose
